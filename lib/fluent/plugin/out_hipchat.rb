de7e111da8761906196948bc4c2d4f469e2e92e8
y-ken <y.ken.studio@gmail.com>
2013-07-15 11:35:18 +0900

    add message formatter method (won't work yet)

--- lib/fluent/plugin/out_hipchat.rb
+++ lib/fluent/plugin/out_hipchat.rb
@@ -12,6 +12,13 @@ module Fluent
     config_param :default_notify, :bool, :default => nil
     config_param :default_format, :string, :default => nil
 
+    config_param :message, :string, :default => nil
+    config_param :out_keys, :string, :default => ""
+    config_param :time_key, :string, :default => nil
+    config_param :tag_key, :string, :default => 'tag'
+
+
+
     attr_reader :hipchat
 
     def initialize
@@ -33,17 +40,17 @@ module Fluent
     def emit(tag, es, chain)
       es.each {|time, record|
         begin
-          send_message(record)
+          send_message(record, tag, time)
         rescue => e
           $log.error("HipChat Error: #{e} / #{e.message}")
         end
       }
     end
 
-    def send_message(record)
+    def send_message(record, tag, time)
       room = record['room'] || @default_room
       from = record['from'] || @default_from
-      message = record['message'] || ''
+      message = record['message'] || evaluate_message(@message, @out_keys, tag, time, record)
       if record['notify'].nil?
         notify = @default_notify
       else
@@ -53,5 +60,23 @@ module Fluent
       message_format = FORMAT.include?(record['format']) ? record['format'] : @default_format
       @hipchat.rooms_message(room, from, message, notify, color, message_format)
     end
+
+  def evaluate_message(message, out_keys, tag, time, record)
+    values = []
+    last = out_keys.length - 1
+
+    values = out_keys.map do |key|
+      case key
+      when @time_key
+        @time_format_proc.call(time)
+      when @tag_key
+        tag
+      else
+        record[key].to_s
+      end
+    end
+
+    (message % values).gsub(/\\n/, "\n")
+  end
   end
 end
